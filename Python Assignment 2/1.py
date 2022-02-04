891a307a8d9b130f80c52cba794935172d3391a7
ramyaPrasadX <ramyaharleyprasadgmail.com>
2022-02-05 02:18:18 +0530

    1st answer

---
+++ Python Assignment 2/1.py
@@ -0,0 +1,7 @@
+"""
+1. Write a program which accepts a sequence of comma-separated numbers from console  and generate a list.
+"""
+
+list = input("enter sequence of comma separated numbers: ")
+list = list.split(',')
+print(list)
